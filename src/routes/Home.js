b16e284c64ab39e873f0d4d34ce6e94fc28e0680
duckyfuz <kenf@iMac&cheese.local>
2023-07-10 23:22:29 +0800

    Add onboarding flow

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Flex } from "@aws-amplify/ui-react";
 
-import { ActivityCardCollection } from "../ui-components";
-
 export const Home = () => {
-  return <Flex justifyContent="center" minWidth={"30rem"}></Flex>;
+  return (
+    <Flex justifyContent="center" minWidth={"30rem"}>
+      Home
+    </Flex>
+  );
 };
--- src/routes/OnBoarding.js
+++ src/routes/OnBoarding.js
@@ -1,7 +1,18 @@
-import React from "react";
-import { Flex, TextField } from "@aws-amplify/ui-react";
+import React, { useEffect } from "react";
+import { Flex } from "@aws-amplify/ui-react";
+import { useNavigate } from "react-router-dom";
+import { useUserObserver } from "../hooks/useUser";
 
 export const OnBoarding = () => {
+  const { user } = useUserObserver();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user && user.onBoarded) {
+      navigate("/dashboard");
+    }
+  }, [navigate, user]);
+
   return (
     <Flex justifyContent="center" minWidth={"30rem"}>
       <div>OnBoarding</div>
